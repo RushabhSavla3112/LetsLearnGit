cef212e9e324dd6dc8820e7d201d9f2cbb6be07b
Rushabh <Rushabhsavla3112@gmail.com>
2022-01-04 17:14:32 +0530

    added msg in sum.java

--- sum.java
+++ sum.java
@@ -3,6 +3,6 @@ class Sum{
     public static void main(String args[]){
         int a=5;
         int b=10;
-         System.out.println(a+b);
+         System.out.println("The Sum of a & b is"+a+b);
     }
 }
